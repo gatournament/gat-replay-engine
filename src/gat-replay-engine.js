20bf1ecb1b27f54973bc9e543bb340875006dfd4
paulocheque <paulocheque@gmail.com>
2013-08-22 01:00:46 -0300

    [new] Card and Deck objects. Basic command parser. and basic test implementation.

--- src/gat-replay-engine.js
+++ src/gat-replay-engine.js
@@ -1,31 +1,55 @@
-var GATReplay = fabric.util.createClass(fabric.Object, {
+if (!String.prototype.trim) {
+  String.prototype.trim=function(){ return this.replace(/^\s+|\s+$/g, ''); };
+  String.prototype.ltrim=function(){ return this.replace(/^\s+/,''); };
+  String.prototype.rtrim=function(){ return this.replace(/\s+$/,''); };
+  String.prototype.fulltrim=function(){ return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' '); };
+}
+
+
+var GATReplay = fabric.util.createClass(fabric.Group, {
   initialize: function(options) {
-    this.callSuper('initialize', options);
-    this.commands = null;
+    options || (options = { });
+    this.callSuper('initialize', options.panels || [], options);
+    this.commands = options.commands || [];
     this._timeBetweenCommands = 1000; // in ms
     this._currentCommand = 0;
     this._execution = null;
   },
 
   nextCommand: function() {
-    if (this.commands != null && this._currentCommand < this.commands.length) {
-      return this.commands[this._currentCommand];
+    if (this.commands != undefined && this.commands != null && this.commands != "" && this._currentCommand < this.commands.length) {
+      var command = this.commands[this._currentCommand];
+      return this.parseCommand(command);
     } else {
       return null;
     }
   },
 
+  parseCommand: function(command) {
+    // console.debug(command);
+    command = command.trim().replace(/\s/g, '');
+    command = command.replace(')', '');
+    tokens = command.split('\(');
+    var cmd = tokens[0];
+    var args = [];
+    if (tokens[1] != undefined) {
+        args = tokens[1].split(',');
+    }
+    return { name: cmd, args: args };
+  },
+
   play: function() {
     if (this._execution != null) return;
+    var that = this;
     this._execution = setInterval(function() {
-      var command = this.nextCommand();
+      var command = that.nextCommand();
       if (command != null) {
-        this.applyCommand(command);
-        this._currentCommand += 1;
+        that._applyCommand(command);
+        that._currentCommand += 1;
       } else {
-        this.pause();
-        this.end();
-        this.stop();
+        that.pause();
+        that._end();
+        that.stop();
       }
     }, this._timeBetweenCommands);
   },
@@ -50,15 +74,103 @@ var GATReplay = fabric.util.createClass(fabric.Object, {
   },
 
   decreaseVelocity: function() {
-    this._timeBetweenCommands = Math.max(this._timeBetweenCommands + 100, 5000);
+    this._timeBetweenCommands = Math.max(this._timeBetweenCommands + 100, 3000);
   },
 
   start: function() {
   },
 
   _applyCommand: function(command) {
+    console.debug(command.name);
+    console.debug(command.args);
   },
 
   _end: function() {
+  },
+});
+
+var Card = fabric.util.createClass(fabric.Group, {
+  SPADES: "♠", // "&#9824;"
+  HEARTS: "♥", // "&#9829;"
+  DIAMONDS: "♦", // "&#9830;"
+  CLUBS: "♣", // "&#9827;"
+  SUITS: { 1: "♠", 2: "♥", 3: "♦", 4: "♣" },
+  AS: 1,
+  J: 11,
+  Q: 12,
+  K: 13,
+  SYMBOLS: { 1: "AS", 11: "J", 12: "Q", 13: "K"},
+
+  initialize: function(options) {
+    options || (options = { });
+    var suit = options.suit.toString();
+    var textColor = "#000";
+    if (suit in this.SUITS) {
+      if (parseInt(suit) == 2 || parseInt(suit) == 2) {
+        textColor = "#f00";
+      }
+      suit = this.SUITS[suit];
+    }
+    var symbol = options.symbol.toString();
+    if (symbol in this.SYMBOLS) {
+      symbol = this.SYMBOLS[symbol];
+    }
+    var cardFont = "Comic Sans";
+    var external = new fabric.Rect({ width: 80, height: 120, rx: 10, ry: 10, fill: "#fff" });
+    var internal = new fabric.Rect({ width: 50, height: 90, rx: 10, ry: 10, fill: "#ccc" });
+
+    var topSuit = new fabric.Text(suit, { fill: textColor, top: -43, left: -33, fontSize: 10, fontWeight: 'bold', fontFamily: cardFont });
+    var bottomSuit = new fabric.Text(suit, { fill: textColor, top: 43, left: 33, angle: 180, fontSize: 10, fontWeight: 'bold', fontFamily: cardFont });
+    var topValue = new fabric.Text(symbol, { fill: textColor, top: -53, left: -33, fontSize: 10, fontWeight: 'bold', fontFamily: cardFont });
+    var bottomValue = new fabric.Text(symbol, { fill: textColor, top: 53, left: 33, angle: 180, fontSize: 10, fontWeight: 'bold', fontFamily: cardFont });
+
+    this.callSuper('initialize', [external, internal, topSuit, topValue, bottomSuit, bottomValue], options);
+  },
+});
+
+var Deck = fabric.util.createClass(fabric.Group, {
+  initialize: function(options) {
+    options || (options = { });
+    this.callSuper('initialize', [], options);
+    this.cardOffset = options.cardOffset || 18;
+    var cards = options.cards || [];
+    for (var i in cards) {
+      var card = cards[i];
+      this.addCard(card);
+    }
+  },
+
+  getCards: function() {
+    return this.getObjects();
+  },
+
+  addCard: function(card) {
+    var i = this.size();
+    card.set('left', (i * this.get('cardOffset')));
+    card.set('angle', (i * 2));
+    this.add(card);
+  },
+
+  removeCard: function(card) {
+    this.remove(card);
+    return card;
+  },
+
+  moveCard: function(card, deck, parent) {
+    this.removeCard(card);
+    parent.add(card);
+    card.set('top', this.getTop());
+    card.animate('angle', 360, {
+      duration: 500,
+    });
+    card.animate('top', deck.getTop(), {
+      duration: 500,
+      onChange: canvas.renderAll.bind(canvas),
+      onComplete: function() {
+        parent.remove(card);
+        deck.addCard(card);
+        canvas.renderAll();
+      }
+    });
   },
 });
--- src/gat-replay-truco.js
+++ src/gat-replay-truco.js
@@ -1,5 +1,20 @@
 var Truco = fabric.util.createClass(GATReplay, {
   initialize: function(options) {
+    var card1 = new Card({ suit: 1, symbol: 1 });
+    var card2 = new Card({ suit: 2, symbol: 11 });
+    var card3 = new Card({ suit: 3, symbol: 12 });
+    var card4 = new Card({ suit: 4, symbol: 13 });
+    var card5 = new Card({ suit: 1, symbol: 5 });
+    var card6 = new Card({ suit: 2, symbol: 6 });
+    var card7 = new Card({ suit: 2, symbol: 8 });
+
+    var card_height = 120;
+    var offset = 20;
+    this.player1 = new Deck({ top: 0, cards: [card1, card2, card3] });
+    this.player2 = new Deck({ top: 2*(card_height+offset), cards: [card4, card5, card6] });
+    this.table = new Deck({ width: 200, height: 200, top: 1*(card_height+offset), backgroundColor: "#ddd", cards: [card7] });
+    this.centerCard = new Card({ top: 1*(card_height+offset), left: -200, suit: 2, symbol: 6 });
+    options.panels = [this.player1, this.player2, this.table, this.centerCard];
     this.callSuper('initialize', options);
   },
 
@@ -7,6 +22,26 @@ var Truco = fabric.util.createClass(GATReplay, {
   },
 
   _applyCommand: function(command) {
+    this.callSuper('_applyCommand', command);
+    switch(command.name) {
+        case "Upcard":
+            var tokens = command.args[0].split("-");
+            var suit = tokens[0];
+            var symbol = tokens[1];
+            this._upcard('player1', suit, symbol);
+            break;
+        case "Truco":
+            break;
+        case "AcceptTruco":
+            break;
+        case "RejectTruco":
+            break;
+    }
+  },
+
+  _upcard: function(player, suit, symbol) {
+    var card = this.player1.getCards()[0];
+    this.player1.moveCard(card, this.table, this);
   },
 
   _end: function() {
